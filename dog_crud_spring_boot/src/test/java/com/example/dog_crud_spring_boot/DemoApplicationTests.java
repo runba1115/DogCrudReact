22cb40666443239584c73e20e005315c48c958c5
fs250309-minoru-takano <runba31@gmail.com>
2025-05-23 20:12:17 +0900

    first commit

---
+++ dog_crud_spring_boot/src/test/java/com/example/dog_crud_spring_boot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dog_crud_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
