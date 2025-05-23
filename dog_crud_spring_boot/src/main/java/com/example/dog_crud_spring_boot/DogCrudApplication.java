4b10d45f933e8642fa90bee93cd589ba23f32644
fs250309-minoru-takano <runba31@gmail.com>
2025-05-23 20:41:51 +0900

    エントリポイントの名前を変えた

--- dog_crud_spring_boot/src/main/java/com/example/dog_crud_spring_boot/DogCrudApplication.java
+++ dog_crud_spring_boot/src/main/java/com/example/dog_crud_spring_boot/DogCrudApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DogCrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DogCrudApplication.class, args);
+    }
 
 }
